280ef49f6ec6a7d0454f4587433cc792d0137ea2
julian villada <julimauvi@gmail.com>
2021-12-08 18:45:06 +0100

    inicio landing page

---
+++ src/landingPage/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './LandingPage.css'
+
+export const LandingPage = () => {
+    return (
+        <div className='LandingPage-Div'>
+            <div>
+            <button>Login</button>
+            <button>Register</button> 
+            </div>
+            
+            <h1>BIENVENIDOS A TIME</h1>
+            <p>Esta es una red social unica y exclusivamente para personas autenticas,aqui experimentaras la maravilla de conocer a personas sabiendo exactamente lo que quieres hacer.</p>
+            
+            <img className='LandingPage-img' src='./image/04-landing.jpg' />
+        </div>
+    )
+};
