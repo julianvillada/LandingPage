9d870ae3a86b951df3938884b8652f267cf953d5
julian villada <julimauvi@gmail.com>
2021-12-13 23:07:20 +0100

    css landing-page buttons and text

--- src/landingPage/LandingPage.js
+++ src/landingPage/LandingPage.js
@@ -6,31 +6,44 @@ export const LandingPage = () => {
     return (
         <div className='LandingPage-Div'>
             <div id='img'>
-            <div>
-                <ul>
-                   <li>TIME</li>
-                   <li>Informacion</li>
-                   <li>Seguridad</li>
-                   <li>Ayuda</li>
-                   <li>Descargar</li> 
-                </ul>
-            </div>
-            <div>
-                <button>Inicia sesión</button>
-            </div>
-            <div>
-            <h1>BIENVENIDOS A TIME</h1>
-            <button>CREA UNA CUENTA</button> 
-            </div>
-            
-            
-            <p>Esta es una red social unica
-                 y exclusivamente para personas autenticas,
-                 aqui experimentaras la maravilla de conocer a personas 
-                 conociendo exactamente lo que quieres hacer.</p>
-            
-            
+                <div className='menu-landingPage'>
+                    <ul>
+                        <li><a href='#'>TIME</a></li>
+                        <li><a href='#'>Información</a></li>
+                        <li><a href='#'>Seguridad</a></li>
+                        <li><a href='#'>Ayuda</a></li>
+                        <li><a href='#'>Descargar</a></li>
+                    <button href="#" className= 'button-iniciaSesion'>
+                       <span id='span1'></span>
+                       <span id='span2'></span>
+                       <span id='span3'></span>
+                       <span id='span4'></span>
+                       Inicia sesión
+                    </button>
+                    </ul>
+              
+                </div>
+           
 
+            
+                <div className='texto-landinPage'>
+                    <h1 >BIENVENIDOS A TIME</h1>
+                    <button href="#" className= 'button-iniciaSesion'>
+                    <span id='span1'></span>
+                    <span id='span2'></span>
+                    <span id='span3'></span>
+                    <span id='span4'></span>
+                    CREA UNA CUENTA
+                    </button> 
+               </div>
+            
+                    <p className='texto-landinPage'>Time única
+                     y exclusivamente para personas auténticas,
+                    experimentaras la maravilla de conocer personas y  
+                    saber exactamente lo que quieres hacer, cuando como y donde. 
+                        <p>¿A QUE SUENA GENIAL?</p>
+                    </p>
+            
             </div>
         </div>
     )
