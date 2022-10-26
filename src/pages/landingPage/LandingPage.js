0d17b1ab357bd31a814637fbe0dd5115a98cb2ec
julian villada <julimauvi@gmail.com>
2022-10-26 22:46:39 +0200

    boton log-reg sin css

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -8,23 +8,23 @@ import { Register } from '../register/Register';
 
 export const LandingPage = () => {
 
-      //Gestión modal registro
-   const [openModalRegister, setOpenModalRegister] = useState(false);
-   const handleOpenRegister = () => setOpenModalRegister(!openModalRegister);
+  //Gestión modal registro
+  const [openModalRegister, setOpenModalRegister] = useState(false);
+  const handleOpenRegister = () => setOpenModalRegister(!openModalRegister);
 
-   const handleCloseRegister = (e) => {
+  const handleCloseRegister = (e) => {
     const { className: el } = e.target;
     if (el !== "backdrop" && el !== "fas fa-times" && el !== "GoToLogin-link")
-      return;
+    return;
     setOpenModalRegister(!openModalRegister);
-  };
+    };
 
 
   //Gestión modal login
   const [openModalLogin, setOpenModalLogin] = useState(false);
   const handleOpenLogin = () => setOpenModalLogin(!openModalLogin);
   const handleCloseLogin = (e) => {
-    const { className: el } = e.target;
+  const { className: el } = e.target;
     if (
       el !== "backdrop" &&
       el !== "fas fa-times" &&
@@ -36,75 +36,57 @@ export const LandingPage = () => {
 
 
 
-    return (
-        <div className='LandingPage-Div'>
-            <div id='img'>
-                <div className='menu-landingPage'>
-                    <ul className='lista-landingPage'>
-                        <li><a>TIME</a></li>
-                        <li><a>Información</a></li>
-                        <li><a>Seguridad</a></li>
-                        <li><a>Ayuda</a></li>
-                        <li><a>Descargar</a></li>
+  return (
+    <div className='LandingPage-Div'>
+      <div id='img'>
 
-
-                   
-                    </ul>
-                    <button 
-                    className= 'button-iniciaSesion'
-                    onClick={handleOpenLogin}
-                    >
-                       <span id='span1'></span>
-                       <span id='span2'></span>
-                       <span id='span3'></span>
-                       <span id='span4'></span>
-                       Inicia sesión
-                    </button> 
-              
-                </div>
-           
-
-            
-                <div className='texto-landinPage'>
-                    <h1 >BIENVENIDOS A TIME</h1>
-                    <button 
-                    className= 'button-iniciaSesion'
-                    onClick={handleOpenRegister}
-                    >
-                    <span id='span1'></span>
-                    <span id='span2'></span>
-                    <span id='span3'></span>
-                    <span id='span4'></span>
-                    CREA UNA CUENTA
-                    </button> 
-               </div>
-            
-                    <p className='texto-landinPage'>Time única
-                     y exclusivamente para personas auténticas,
-                    experimentaras la maravilla de conocer personas y  
-                    saber exactamente lo que quieres hacer, cuando como y donde. 
-                        <p>¿A QUE SUENA GENIAL?</p>
-                    </p>
-
-                    {openModalRegister && (
-        <Modal handleClose={handleCloseRegister}>
-          <Register
-            handleCloseRegister={handleCloseRegister}
-            openLogin={handleOpenLogin}
-          />
-        </Modal>
-      )}
-
-      {openModalLogin && (
-        <Modal handleClose={handleCloseLogin}>
-          <Login
-            handleCloseLogin={handleCloseLogin}
-            openRegister={handleOpenRegister}
-          />
-        </Modal>
-      )}
-            
-            </div>
+        <div className='menu-landingPage'>
+          <ul className='lista-landingPage'>
+            <li><a>TIME</a></li>
+            <li><a>Información</a></li>
+            <li><a>Seguridad</a></li>
+            <li><a>Ayuda</a></li>
+            <li><a>Descargar</a></li>
+            <button className='button-iniciaSesion'onClick={handleOpenLogin} > Inicia sesión </button>
+          </ul>
         </div>
-    )
+
+
+
+        <div className='texto-landinPage'>
+          <h1 >BIENVENIDOS A TIME</h1>
+          <button className='button-register' onClick={handleOpenRegister} > Crea una cuenta </button>
+        </div>
+
+        <div>
+        <p className='texto-landinPage'>Time única
+          y exclusivamente para personas auténticas,
+          experimentaras la maravilla de conocer personas y
+          saber exactamente lo que quieres hacer, cuando como y donde.
+          <p>¿A QUE SUENA GENIAL?</p>
+        </p>        
+        </div>
+        
+
+        {openModalRegister && (
+          <Modal handleClose={handleCloseRegister}>
+            <Register
+              handleCloseRegister={handleCloseRegister}
+              openLogin={handleOpenLogin}
+            />
+          </Modal>
+        )}
+
+        {openModalLogin && (
+          <Modal handleClose={handleCloseLogin}>
+            <Login
+              handleCloseLogin={handleCloseLogin}
+              openRegister={handleOpenRegister}
+            />
+          </Modal>
+        )}
+
+      </div>
+    </div>
+  )
 };
